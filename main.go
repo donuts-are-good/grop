91fb143c715b6e455527745369806ae8e5b3e729
donuts-are-good <96031819+donuts-are-good@users.noreply.github.com>
2023-05-10 00:47:15 -0500

    hello grop

--- main.go
+++ main.go
@@ -1,5 +1,60 @@
 package main
 
+import (
+	"bufio"
+	"fmt"
+	"io/fs"
+	"os"
+	"path/filepath"
+	"strings"
+)
+
 func main() {
-	println("alive!")
+	if len(os.Args) < 3 {
+		fmt.Println("Usage: ./grop \"keyword\" /path/to/directory")
+		return
+	}
+
+	keyword := os.Args[1]
+	path := os.Args[2]
+
+	err := filepath.Walk(path, func(path string, info fs.FileInfo, err error) error {
+		if err != nil {
+			return err
+		}
+
+		if !info.IsDir() {
+			searchInFile(keyword, path)
+		}
+
+		return nil
+	})
+
+	if err != nil {
+		fmt.Println("Error walking the path:", err)
+	}
+}
+
+func searchInFile(keyword, path string) {
+	file, err := os.Open(path)
+	if err != nil {
+		fmt.Println("Error opening file:", err)
+		return
+	}
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+	lineNumber := 1
+
+	for scanner.Scan() {
+		line := scanner.Text()
+		if strings.Contains(line, keyword) {
+			fmt.Printf("%s -> line %d: \"%s\"\n", path, lineNumber, line)
+		}
+		lineNumber++
+	}
+
+	if err := scanner.Err(); err != nil {
+		fmt.Println("Error reading file:", err)
+	}
 }
